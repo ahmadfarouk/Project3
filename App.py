df8a6c1040c7b28cf28e91b07d9bf429c6c0ce76
Loic Tiemani <loictiems@github.com>
2021-02-02 19:47:31 -0500

    Pushing structure

--- App.py
+++ App.py
@@ -1,11 +1,21 @@
 import os
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
-from flask import  ( 
+from flask import( 
     Flask, 
     render_template,
     jsonify,
     request,
-    redirect
-)
+    redirect)
 
-from flask_sqlalchemy import flask_sqlalchemy
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+#from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
