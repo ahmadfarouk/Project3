4f8de06a0bc61b31b16cdcbc1a5193e34eabe812
Loic Tiemani <loictiems@github.com>
2021-02-02 20:04:57 -0500

    Updating structure

--- App.py
+++ App.py
@@ -19,3 +19,25 @@ import datetime as dt
 #from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+dictionary = {
+    "ticker": "Amzn"
+
+}
+
+## FRONT_END ROUTES
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+## SERVICE ROUTES
+@app.route("/api/main/netflix")
+def wel():
+    
+    return jsonify(dictionary)
+
+
+# 4. Define main behavior
+if __name__ == "__main__":
+    app.run(debug=True)     
