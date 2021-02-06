93dc80f1d28660a1a1df1cfd90380802ff0ef21d
ahmadfarouk <49321942+ahmadfarouk@users.noreply.github.com>
2021-02-06 10:44:28 -0500

    clean up after work submitted by Loic

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -8,11 +8,12 @@ from sqlalchemy import create_engine, func,inspect
 from sqlalchemy import Integer, Column, Float, String
 import datetime as dt
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("postgres://pgadmin@pg-srv-001:ucN-xZRL3NsaBjvG2tcw1gPcsNeS5Xfw@pg-srv-001.postgres.database.azure.com:5432/netflix",connect_args={'sslmode':'require'})
+conn=engine.connect()
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-
 #print (Base.classes.keys())
+
 title = Base.classes.title
 country = Base.classes.country
 listed_in = Base.classes.listed_in
@@ -26,26 +27,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index_ahmad.html")
+    return render_template("index.html")
 
 @app.route("/api/v1.0/titles")
 def titles():
-    return jsonify(allStations)
-
-
-   allTitles = []
+    allTitles = []
     Titles = session.query(title.title,title.description,title.date_added).all()
     for result2 in Titles:
         row = {}
         row["title"] = result2[0]
         row["description"] = result2[1]
         row["date_added"] = result2[2]
-#        row["longitude"] = result2[3]
-#        row["elevation"] = result2[4]
+#         row["longitude"] = result2[3]
+#         row["elevation"] = result2[4]
         allTitles.append(row)
     return jsonify(allTitles)
 
-   
-    app.run(debug=True)
 # 4. Define main behavior
 if __name__ == "__main__":
+    app.run(debug=True)
--- myFlaskApp/static/js/app-ahmad.js
+++
@@ -1,3 +0,0 @@
-d3.json("/api/v1.0/titles").then(function(data) {
-    console.log(data)
-})
