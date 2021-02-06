f3c09d5a0140fea2ebda61d9dad9ca912ff16e38
Loic Tiemani <loictiems@github.com>
2021-02-06 10:27:20 -0500

    Update app-ahmad.py

--- app-ahmad.py
+++ app-ahmad.py
@@ -41,32 +41,7 @@ def titles():
         row["description"] = result2[1]
         row["date_added"] = result2[2]
 
-    for result1 in Precipitation_12months:
-        row = {}
-        row["date"] = result1[0]
-        row["prcp"] = result1[1]
-        prcp_totals.append(row)
-
-    Stations_all = session.query(Station.station,Station.name, Station.latitude,Station.longitude, Station.elevation).all()
-    for result2 in Stations_all:
-        row = {}
-        row["station"] = result2[0]
-        row["name"] = result2[1]
-        row["latitude"] = result2[2]
-        row["longitude"] = result2[3]
-        row["elevation"] = result2[4]
-        allStations.append(row)
-
-    station_most_active_lastYear = session.query(Measurement.station, func.count(Measurement.tobs)).filter(Measurement.date > one_year_date).group_by(Measurement.station).order_by(func.count(Measurement.tobs).desc()).first()
-    station_most_active_tobs = session.query(Measurement.station, Measurement.tobs)\
-        .filter(Measurement.date > one_year_date).filter(Measurement.station == station_most_active_lastYear[0]).all()
-
-    for result3 in station_most_active_tobs:
-        row = {}
-        row["station"] = result3[0]
-        row["tobs"] = result3[1]
-        tobs_station_most_active.append(row)
-
+   
     app.run(debug=True)
 # 4. Define main behavior
 if __name__ == "__main__":
