6429cccc64d01433fddd4a2b2fa7aa55f77a5f99
hamidzar <hamid.zarringhalam@gmail.com>
2021-02-06 13:22:53 -0500

    Update app.py

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -1,36 +1,30 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func,inspect
 from sqlalchemy import Integer, Column, Float, String
 import datetime as dt
-
 engine = create_engine("postgres://pgadmin@pg-srv-001:ucN-xZRL3NsaBjvG2tcw1gPcsNeS5Xfw@pg-srv-001.postgres.database.azure.com:5432/netflix",connect_args={'sslmode':'require'})
-conn=engine.connect()
+conn = engine.connect()
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 #print (Base.classes.keys())
-
 title = Base.classes.title
 country = Base.classes.country
 listed_in = Base.classes.listed_in
 players = Base.classes.players
 pg_rating = Base.classes.pg_rating
 director = Base.classes.director
-number_of_titles_per_country = Base.classes.number_of_titles_per_country
-
-
+title = Base.classes.title
+title_country = Base.classes.title_country
+country = Base.classes.country
 session = Session(engine)
-
 app = Flask(__name__)
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
 @app.route("/api/v1.0/titles")
 def titles():
     allTitles = []
@@ -44,37 +38,30 @@ def titles():
 #         row["elevation"] = result2[4]
         allTitles.append(row)
     return jsonify(allTitles)
-
-
 @app.route("/api/v1.0/countries")
 def countries():
     allCountries = []
     Countries = session.query(country.country_id, country.country_name, country.longitude, country.latitude).all()
-    
     for result3 in Countries:
         row = {}
         row["country_id"] = result3[0]
         row["country_name"] = result3[1]
         row["longitude"] = result3[2]
         row["latitude"] = result3[3]
-       
 #         row["elevation"] = result2[4]
         allCountries.append(row)
     return jsonify(allCountries)
-
 @app.route("/api/v1.0/titles_country")
 def titles_country():
     titles_countries= []
-    countries_count = session.query(number_of_titles_per_country.count, number_of_titles_per_country.country_name).all()
-    
+    #countries_count = session.query (title.show_id, title.title,title_country.show_id, country.country_name, country.country_id).filter(title.show_id==title_country.show_id,).filter(title_country.country_id == country.country_id).all()
+    countries_count = session.query (country.country_name, func.count(title.show_id)).filter(title.show_id==title_country.show_id,).filter(title_country.country_id == country.country_id).group_by(country.country_name).all()
     for result4 in countries_count:
         row = {}
-        row["count per country"] = result4[0]
-        row["country name"] = result4[1]
-       
+        row["Count of Titles"] = result4[1]
+        row["country name"] = result4[0]
         titles_countries.append(row)
     return jsonify(titles_countries) 
-
 # 4. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
