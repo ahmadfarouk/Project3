2394288133b2ab65a4ecb82bdd054dfbe2de8833
ahmadfarouk <49321942+ahmadfarouk@users.noreply.github.com>
2021-02-09 16:18:19 -0500

    adding more routes

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -5,7 +5,7 @@ import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func,inspect
+from sqlalchemy import create_engine, func,inspect, desc
 from sqlalchemy import Integer, Column, Float, String
 
 engine = create_engine("postgres://pgadmin@pg-srv-001:ucN-xZRL3NsaBjvG2tcw1gPcsNeS5Xfw@pg-srv-001.postgres.database.azure.com:5432/netflix",connect_args={'sslmode':'require'})
@@ -18,6 +18,7 @@ title = Base.classes.title
 country = Base.classes.country
 listed_in = Base.classes.listed_in
 players = Base.classes.players
+player_title = Base.classes.player_title
 pg_rating = Base.classes.pg_rating
 director = Base.classes.director
 title = Base.classes.title
@@ -37,13 +38,11 @@ def index():
 def titles():
     allTitles = []
     Titles = session.query(title.title,title.description,title.date_added).all()
-    for result2 in Titles:
+    for result in Titles:
         row = {}
-        row["title"] = result2[0]
-        row["description"] = result2[1]
-        row["date_added"] = result2[2]
-        #row["country"] = result2[3]
-#         row["elevation"] = result2[4]
+        row["title"] = result[0]
+        row["description"] = result[1]
+        row["date_added"] = result[2]
         allTitles.append(row)
     return jsonify(allTitles)
 
@@ -51,13 +50,12 @@ def titles():
 def countries():
     allCountries = []
     Countries = session.query(country.country_id, country.country_name, country.longitude, country.latitude).all()
-    for result3 in Countries:
+    for result in Countries:
         row = {}
-        row["country_id"] = result3[0]
-        row["country_name"] = result3[1]
-        row["longitude"] = result3[2]
-        row["latitude"] = result3[3]
-#         row["elevation"] = result2[4]
+        row["country_id"] = result[0]
+        row["country_name"] = result[1]
+        row["longitude"] = result[2]
+        row["latitude"] = result[3]
         allCountries.append(row)
     return jsonify(allCountries)
 
@@ -66,11 +64,11 @@ def countries():
 def titles_country():
     titles_countries_years= []
     countries_count = session.query (country.country_name, title.release_year, func.count(title.show_id)).filter(title.show_id==title_country.show_id,).filter(title_country.country_id == country.country_id).group_by(country.country_name, title.release_year).all()
-    for result4 in countries_count:
+    for result in countries_count:
         row = {}
-        row["country_name"] = result4[0]
-        row["release_year"] = result4[1]
-        row["Count_of_Titles"] = result4[2]
+        row["country_name"] = result[0]
+        row["release_year"] = result[1]
+        row["Count_of_Titles"] = result[2]
         titles_countries_years.append(row)
     return jsonify(titles_countries_years)
 
@@ -79,10 +77,10 @@ def titles_country():
 def budget_revenue():
     budget_revenue_all= []
     budget_revenue = session.query (title.budget, title.revenue).filter(title.budget!=0,).filter(title.revenue != 0).all()
-    for result5 in budget_revenue:
+    for result in budget_revenue:
         row = {}
-        row["Budget"] = result5[0]
-        row["Revenue"] = result5[1]
+        row["Budget"] = result[0]
+        row["Revenue"] = result[1]
         budget_revenue_all.append(row)
     return jsonify(budget_revenue_all)
 
@@ -91,10 +89,10 @@ def budget_revenue():
 def pgrating_totalbudget():
     pgrating_totalbudget_all= []
     pgrating_totalbudget = session.query (pg_rating.pg_rating_name, func.sum(title.budget)).filter(title.pg_rating_id==pg_rating.pg_rating_id).group_by(pg_rating.pg_rating_name).all()
-    for result6 in pgrating_totalbudget:
+    for result in pgrating_totalbudget:
         row = {}
-        row["PG_Rating"] = result6[0]
-        row["Total_budget"] = result6[1]
+        row["PG_Rating"] = result[0]
+        row["Total_budget"] = result[1]
         pgrating_totalbudget_all.append(row)
     return jsonify(pgrating_totalbudget_all)
 
@@ -103,10 +101,10 @@ def pgrating_totalbudget():
 def country_budget():
     country_budget_all= []
     country_budget = session.query (country.country_name, func.sum(title.budget)).filter(title.show_id == title_country.show_id,).filter(title_country.country_id == country.country_id).group_by(country.country_name).all()
-    for result7 in country_budget:
+    for result in country_budget:
         row = {}
-        row["country_name"] = result7[0]
-        row["budget"] = result7[1]
+        row["country_name"] = result[0]
+        row["budget"] = result[1]
         country_budget_all.append(row)
     return jsonify(country_budget_all)
 
@@ -115,10 +113,10 @@ def country_budget():
 def listedin_count():
     listedin_count_all= []
     listedin_count = session.query (listed_in.listed_in_name, func.count(title.show_id)).filter(title.show_id == listed_in_title.show_id,).filter(listed_in.listed_in_id == listed_in_title.listed_in_id).group_by(listed_in.listed_in_name).all()
-    for result8 in listedin_count:
+    for result in listedin_count:
         row = {}
-        row["Listed_in_name"] = result8[0]
-        row["Count"] = result8[1]
+        row["Listed_in_name"] = result[0]
+        row["Count"] = result[1]
         listedin_count_all.append(row)
     return jsonify(listedin_count_all)
 
@@ -126,14 +124,40 @@ def listedin_count():
 @app.route("/api/v1.0/directors_count")
 def directors_count():
     directors_count_all= []
-    directors_count = session.query (director.director_name, func.count(title.show_id)).filter(director.director_id == director_title.director_id,).filter(title.show_id == director_title.show_id).group_by(director.director_name).all()
-    for result9 in directors_count:
+    directors_count = session.query (director.director_name, title.release_year, func.count(title.show_id)).filter(director.director_id == director_title.director_id,).filter(title.show_id == director_title.show_id).group_by(director.director_name, title.release_year).all()
+    for result in directors_count:
         row = {}
-        row["director_name"] = result9[0]
-        row["Count"] = result9[1]
+        row["director_name"] = result[0]
+        row["release_year"] = result[1]
+        row["Count"] = result[2]
         directors_count_all.append(row)
     return jsonify(directors_count_all)
 
+####x: Directors vs Revenue
+@app.route("/api/v1.0/directors_revenue")
+def directors_revenue():
+    directors_revenue_all= []
+    directors_revenue = session.query (director.director_name, title.release_year, func.sum(title.revenue)).filter(director.director_id == director_title.director_id,).filter(title.show_id == director_title.show_id).group_by(director.director_name, title.release_year).all()
+    for result in directors_revenue:
+        row = {}
+        row["director_name"] = result[0]
+        row["release_year"] = result[1]
+        row["revenue"] = result[2]
+        directors_revenue_all.append(row)
+    return jsonify(directors_revenue_all)
+
+####x: year vs y: revenue
+@app.route("/api/v1.0/release_year_revenue")
+def release_year_revenue():
+    release_year_revenue_all= []
+    release_year_revenue = session.query (title.release_year, players.player_name, func.sum(title.revenue)).filter(player_title.show_id == title.show_id,).filter(players.player_id == player_title.player_id).group_by(players.player_name,title.release_year).order_by(desc(func.sum(title.revenue))).all()
+    for result in release_year_revenue:
+        row = {}
+        row["year"] = result[0]
+        row["player_name"] = result[1]
+        row["revenue"] = result[2]
+        release_year_revenue_all.append(row)
+    return jsonify(release_year_revenue_all)
 
 # 4. Define main behavior
 
