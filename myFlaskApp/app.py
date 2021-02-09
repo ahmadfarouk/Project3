bd7cec00a9161c19833d3f88e735285f9ba74b04
hamidzar <hamid.zarringhalam@gmail.com>
2021-02-08 20:34:56 -0500

    Update app.py

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
 import datetime as dt
-​
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func,inspect
 from sqlalchemy import Integer, Column, Float, String
-​
+
 engine = create_engine("postgres://pgadmin@pg-srv-001:ucN-xZRL3NsaBjvG2tcw1gPcsNeS5Xfw@pg-srv-001.postgres.database.azure.com:5432/netflix",connect_args={'sslmode':'require'})
 conn = engine.connect()
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 #print (Base.classes.keys())
-​
+
 title = Base.classes.title
 country = Base.classes.country
 listed_in = Base.classes.listed_in
@@ -23,14 +23,14 @@ director = Base.classes.director
 title = Base.classes.title
 title_country = Base.classes.title_country
 country = Base.classes.country
-​
+
 session = Session(engine)
-​
+
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-​
+
 @app.route("/api/v1.0/titles")
 def titles():
     allTitles = []
@@ -44,7 +44,7 @@ def titles():
 #         row["elevation"] = result2[4]
         allTitles.append(row)
     return jsonify(allTitles)
-​
+
 @app.route("/api/v1.0/countries")
 def countries():
     allCountries = []
@@ -58,7 +58,7 @@ def countries():
 #         row["elevation"] = result2[4]
         allCountries.append(row)
     return jsonify(allCountries)
-​
+
 ####Country vs y: Count of Titles, drop down: Year ----done
 @app.route("/api/v1.0/titles_country")
 def titles_country():
@@ -71,7 +71,7 @@ def titles_country():
         row["Count_of_Titles"] = result4[2]
         titles_countries_years.append(row)
     return jsonify(titles_countries_years)
-​
+
 ####x: Budget vs y: Movie Revenue 
 @app.route("/api/v1.0/budget_revenue")
 def budget_revenue():
@@ -83,7 +83,7 @@ def budget_revenue():
         row["Revenue"] = result5[1]
         budget_revenue_all.append(row)
     return jsonify(budget_revenue_all)
-​
+
 ####x: Budget vs y: rating
 @app.route("/api/v1.0/pgrating_totalbudget")
 def pgrating_totalbudget():
@@ -95,14 +95,13 @@ def pgrating_totalbudget():
         row["Total_budget"] = result6[1]
         pgrating_totalbudget_all.append(row)
     return jsonify(pgrating_totalbudget_all)
-​
-​
+
+
 ####x: Budget vs y: Country
-​
-​
-​
+
+
+
 # 4. Define main behavior
-​
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
