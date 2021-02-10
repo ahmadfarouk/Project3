a57ad526a03a6bc55926176a0375843744005c24
hamidzar <hamid.zarringhalam@gmail.com>
2021-02-10 13:37:01 -0500

    Update app3.js

--- myFlaskApp/static/js/app3.js
+++ myFlaskApp/static/js/app3.js
@@ -86,25 +86,3 @@ function readData(sample){
     };
     //readData();
     
-function init() {
-    drop_down=d3.select('#selDataset');
-
-    d3.json("/api/v1.0/titles_country").then((data) => {
-        //loop through ids_selection and append option to drop_down
-        data.forEach((item)=>
-        {
-            drop_down.append('option').text(item.release_year).property("value", item.release_year);
-        });
-
-        //grab the first sample and build the charts on the page for page load
-        firstRelease_year=data[0].release_year
-        //console.log(firstCountry)
-        buildPlot(data, firstRelease_year);
-        // readData(firstSample);
-        // buildGauge(firstSample);
-    })
-}
-
-
-//call init for page load
-init();
