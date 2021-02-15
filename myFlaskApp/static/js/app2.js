69902ece93566c0eee1e4238c4d0b660e27b1f44
Loic Tiemani <loictiems@github.com>
2021-02-14 22:47:45 -0500

    Update app2.js

--- myFlaskApp/static/js/app2.js
+++ myFlaskApp/static/js/app2.js
@@ -14,7 +14,23 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
+  // Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+  // Parameters
+
+  var labelXaxis = "Budget";
+  var labelYaxis = "Revenue"
 
             
  
