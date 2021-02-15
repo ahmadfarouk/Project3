a8ef8167a57d292826f13dd3c35e002bf0f9e76c
Loic Tiemani <loictiems@github.com>
2021-02-14 22:46:48 -0500

    Update app2.js

--- myFlaskApp/static/js/app2.js
+++ myFlaskApp/static/js/app2.js
@@ -3,21 +3,4 @@
 
 
             
-            
-   
-        //console.log(firstCountry)
-        buildPlot(data, firstRelease_year);
-        // readData(firstSample);
-        // buildGauge(firstSample);
-    })
-}
-
-/*function optionChanged (newSample){
-    //Fetch New Data Each time a New sample is selcted
-    buildPlot(newSample);
-    readData(newSample);
-    buildGauge(newSample);
-}*/
-
-//call init for page load
-init();
+ 
