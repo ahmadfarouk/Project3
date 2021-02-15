9344e2c951ca4930eef44385dce8de2844b3bcc0
Loic Tiemani <loictiems@github.com>
2021-02-14 22:47:16 -0500

    Update app2.js

--- myFlaskApp/static/js/app2.js
+++ myFlaskApp/static/js/app2.js
@@ -1,4 +1,18 @@
+// @TODO: YOUR CODE HERE!
 
+
+var svgWidth = 1000;
+var svgHeight = 600;
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+// Define Dimensions of the Chart Area
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
 
 
