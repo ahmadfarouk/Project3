67205bf4cc6acb247db9119d2e75fa9bcd3bc83f
Loic Tiemani <loictiems@github.com>
2021-02-14 22:51:55 -0500

    Update app2.js

--- myFlaskApp/static/js/app2.js
+++ myFlaskApp/static/js/app2.js
@@ -23,7 +23,7 @@ var svg = d3
   .attr("height", svgHeight);
 
   // Append an SVG group
-var chartGroup = svg.append("g")
+  var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
@@ -39,7 +39,7 @@ function xScale(data, labelXaxis) {
     .domain([d3.min(data, d=> d[labelXaxis])*0.9,  d3.max(data, d => d[labelXaxis])*1.1])
     .range([0, width]);
     return xLinearScale;
-  }
+}
  
   // function used for updating y-scale var upon click on axis label
  function yScale(data, labelYaxis) {
@@ -48,7 +48,7 @@ function xScale(data, labelXaxis) {
      .domain([d3.min(data, d => d[labelYaxis])*0.9, d3.max(data, d => d[labelYaxis])*1.1])
      .range([height, 0]);
  
-   return yLinearScale; 
+    return yLinearScale; 
 }
 // function used for updating xAxis var upon click on axis label
 function renderAxesX(newXScale, xAxis) {
@@ -70,7 +70,7 @@ function renderAxesY(newYScale, yAxis) {
       .call(leftAxis);
   
     return yAxis;
-  }
+}
 
   // function used for updating circles group with a transition to
 // new circles
@@ -82,7 +82,7 @@ function renderCircles(circlesGroup, newXScale, labelXaxis, newYScale, labelYaxi
       .attr("cy", d => newYScale(d[labelYaxis]));
   
     return circlesGroup;
-  }
+}
 
   function renderCirclesText(circlesText, newXScale, labelXaxis, newYScale, labelYaxis) {
 
@@ -92,6 +92,31 @@ function renderCircles(circlesGroup, newXScale, labelXaxis, newYScale, labelYaxi
       .attr("y", d => newYScale(d[labelYaxis]));
   
     return circlesText;
-  }
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(labelXaxis, labelYaxis, circlesGroup) {
+
+    if (labelXaxis === "Budget") {
+      var labelX = "Budget: ";
+    }
+    else if (labelXaxis==="Total_budget"){
+      var labelY="Total_budget: ";
+  
+    }
+  
+      else if (labelXaxis==="budget"){
+        var labelY="budget: ";
+    }
+    if (labelYaxis==="Revenue"){
+      var labelY="Revenue: "
+    }
+    else if (labelYaxis==="PG_rating"){
+      var labelY="PG_rating: ";
+  
+    }
+    else if (labelYaxis==="country_name"){
+      var labelY="country_name: ";
+    } 
   
   
