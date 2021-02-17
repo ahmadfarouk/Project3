ba7ef06ed4298a9e2c7eb1b4e737ca2aee606333
Loic Tiemani <loictiems@github.com>
2021-02-16 23:55:33 -0500

    Update app2.js

--- myFlaskApp/static/js/app2.js
+++ myFlaskApp/static/js/app2.js
@@ -20,4 +20,63 @@
    }  
  function init_svg ()
 
+ {
+
+  var svg = d3
+    .select("#bar")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+  // Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+ return chartGroup
+ }
+
+  // Parameters
+
+  var labelXaxis = "TitleCount";
+  // var labelXaxis = "TotalRevenue";
+  // var factor = 100000000
+
+// function used for updating X-Scale var upon click
+ function xScale(TitleData, labelXaxis) {
+   // create scales
+   var xLinearScale = d3.scaleLinear()
+   .domain([d3.min(TitleData, d => d[labelXaxis])* 0.1, 
+   d3.max(TitleData, d => d[labelXaxis]) * 1.2])
+   .range([0, width]);
+
+   return xLinearScale;
+ }
+
+ // function used for updating y-scale var upon click on axis label
+// function yScale(data,labelYaxis) {
+  // create scales 
  
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// // function used for updating yAxis var upon click on axis label
+// function renderAxesY(newYScale, yAxis) {
+//   var leftAxis = d3.axisLeft(newYScale);
+
+//   yAxis.transition()
+//     .duration(1000)
+//     .call(leftAxis);
+
+// //   return yAxis;
+// }
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, labelXaxis) {
