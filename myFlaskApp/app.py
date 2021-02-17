cb0df4e4db6dbbcbf67380197660fcc5247a288a
hamidzar <hamid.zarringhalam@gmail.com>
2021-02-16 21:22:40 -0500

    Update

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -36,11 +36,11 @@ def index():
 
 @app.route("/index1")
 def index1():
-    return render_template("index3.html")
+    return render_template("index1.html")
 
 @app.route("/index2")
 def index2():
-    return render_template("index3.html")
+    return render_template("index2.html")
 
 ####index3
 @app.route("/index3")
