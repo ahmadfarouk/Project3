88483457e390dfa01d48a1b0190dd2f27f32909a
Loic Tiemani <loictiems@github.com>
2021-02-16 23:52:57 -0500

    Update app2.js

--- myFlaskApp/static/js/app2.js
+++ myFlaskApp/static/js/app2.js
@@ -1,21 +1,4 @@
-// @TODO: YOUR CODE HERE!
 
-
-var svgWidth = 1000;
-var svgHeight = 600;
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
-};
-
-// Define Dimensions of the Chart Area
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
 var svg = d3
   .select("body")
   .append("svg")
